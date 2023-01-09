460a57b1a9294be6c663e72e7a636bbf81418c08
lazca2080 <lazca2080@gmail.com>
2023-01-09 19:50:47 +0900

    2023/01/09

---
+++ User/src/main/java/kr/co/user/vo/User1VO.java
@@ -0,0 +1,18 @@
+package kr.co.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User1VO {
+	
+	private String uid;
+	private String name;
+	private String hp;
+	private int age;
+}
---
+++ User/src/main/java/kr/co/user/vo/User2VO.java
@@ -0,0 +1,17 @@
+package kr.co.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User2VO {
+	private String uid;
+	private String name;
+	private String hp;
+	private int age;
+}
