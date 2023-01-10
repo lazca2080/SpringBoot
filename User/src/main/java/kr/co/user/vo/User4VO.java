37740eba712dfe3ae02992cbc6a934bc637426d9
lazca2080 <lazca2080@gmail.com>
2023-01-10 18:26:29 +0900

    2023/01/10

---
+++ User/src/main/java/kr/co/user/vo/User4VO.java
@@ -0,0 +1,18 @@
+package kr.co.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User4VO {
+	private int seq;
+	private String name;
+	private int gender;
+	private int age;
+	private String addr;
+}
