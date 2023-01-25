11f8c7d1410ee86ead525b86c1287ee16dfd1420
lazca2080 <lazca2080@gmail.com>
2023-01-25 18:52:08 +0900

    2023/01/25

---
+++ FarmStory/src/main/java/kr/co/farmstory/FarmStoryApplication.java
@@ -0,0 +1,12 @@
+package kr.co.farmstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmStoryApplication {
+
+	public static void main(String[] args) { SpringApplication.run(FarmStoryApplication.class, args); }
+
+
+}
---
+++ FarmStory/src/test/java/kr/co/farmstory/FarmStory/FarmStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.farmstory.FarmStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
